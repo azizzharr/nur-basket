9b28bff28391558b09f77888039db83378b0170d
Ваше Имя <azizzharr>
2020-09-26 20:47:05 +0600

    dfgh

--- src/component/basket.js
+++ src/component/basket.js
@@ -2,12 +2,18 @@ import React from 'react';
 import BasketItem from "./basket-item";
 
 const Basket = ({basket,plusProduct,deleteProduct}) => {
+    const total = basket.reduce((ans,item) => {
+        return ans + item.sum
+    },0)
     return (
         <section>
             <div className="container jumbotron">
                 {basket.map((item) => {
                     return <BasketItem plusProduct={plusProduct} deleteProduct={deleteProduct} key={item.id} {...item}/>
                 })}
+                <p style={{textAlign:'right'}}>
+                    Total:{total}
+                </p>
             </div>
         </section>
     );
